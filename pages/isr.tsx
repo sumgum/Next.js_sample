4b8dbbe5776bda08a84938a3cc58ba06d48d05aa
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-13 11:39:38 +0900

    ISR

---
+++ pages/isr.tsx
@@ -0,0 +1,46 @@
+import { GetStaticProps, NextPage, GetStaticPaths } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+type ISRProps = {
+  message: string;
+};
+
+// ISRPropsを受け付けるNextPage(ページ)の型
+const ISR: NextPage<ISRProps> = (props) => {
+  const { message } = props;
+
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Creat Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>このページはISRによってビルド時に生成されたページです。</p>
+        <p>{message}</p>
+      </main>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<ISRProps> = async (context) => {
+  const timestamp = new Date().toLocaleString();
+  const message = `${timestamp} にこのページのgetStaticPropsが実行されました。`;
+
+  return {
+    props: {
+      message: message,
+    },
+    // このページの有効期間を秒単位で指定
+    revalidate: 60,
+  };
+};
+
+export default ISR;
